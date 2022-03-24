cdcd94d1d80aa5f9993bc57854345a94ce2b9b3e
kang-narae <96982110+kang-narae@users.noreply.github.com>
2022-03-24 21:43:04 +0900

    ML

--- tensorflow/dd.py
+++
@@ -1,8 +0,0 @@
-
-import requests
-
-url = 'http://apis.data.go.kr/B552895/LocalGovPriceInfoService/getItemPriceResearchSearch'
-params ={'serviceKey' : 'kPvWkLn8zX/rcSryaV88GKZw89YENVfrgvH06AuvYSrXsHOx6r705chjRSn/+jrdwYpeOPms5YcVRuYID5eWkA==', 'numOfRows' : '3', 'pageNo' : '1', '_returnType' : 'xml', 'examin_de' : '20200504' }
-
-response = requests.get(url, params=params)
-print(response.content)
